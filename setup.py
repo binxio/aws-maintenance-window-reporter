17ef5f48acf4ae462f99689318bdfec5a9e8d24e
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-03-03 16:27:33 +0100

    bumped to version 0.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aws-maintenance-window-reporter',
-    version="0.1.6",
+    version="0.1.7",
     url='https://github.com/binxio/aws-maintenance-window-reporter',
     license='BSD',
     author='Mathijs van Mourick',
