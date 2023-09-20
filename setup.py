439c39f56a9b5038511b198e242e63a1f5be3d18
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2023-09-20 15:03:39 +0200

    upgrade to Python 3.9 - bumped to release 0.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aws-maintenance-window-reporter',
-    version="0.2.8",
+    version="0.3.0",
     url='https://github.com/binxio/aws-maintenance-window-reporter',
     license='BSD',
     author='Mathijs van Mourick',
